ab587ea184979e4ffcf4c63d6eb67ce38b9f4e46
Cassie Esposito <dev@timevault.org>
2021-10-04 14:28:03 -0700

    Added preverification via HTML headers

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Image Validator
-CGI script that recieves the URL of an image file as a parameter, checks that it's below a given size and that its file type is acceptable. Useful for validating files before allowing a user to submit them. 
+CGI script that recieves the URL of an image file as a parameter, checks the http headers to see if it's reported as below a given size and that the mime type is of an acceptable type. Then it downloads the file to the server, verifies the image is actually of that type and an acceptable size. This could be useful for validating files before allowing a user to submit them. It could be extended to non-image formats by using the python-magic library instead of built in library imghdr, but that adds additional requirements, so I haven't done that. Yet.
 
 ## Defaults
 To change defaults, edit constants in `./cgi-bin/imageValidator.py`
 Maximum file size: 4 MiB
-Allowed file formats: jpeg, png
+Allowed file formats: jpeg, png, gif
 
 Check [https://docs.python.org/3/library/imghdr.html](https://docs.python.org/3/library/imghdr.html) for a list of file formats that can be detected.
 
--- cgi-bin/imageValidator.py
+++ cgi-bin/imageValidator.py
@@ -8,8 +8,11 @@ import io
 
 # Many web hosts will refuse to respond to requests from a script. Sending a false UA solves this.
 USER_AGENT = "Mozilla/5.0 (Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0"
+HEADERS = {"User-Agent": USER_AGENT}
 MAX_FILE_SIZE_IN_BYTES = 1024 * 1024 * 4
-ACCEPTABLE_IMAGE_TYPES = ["jpeg", "png"]
+ACCEPTABLE_MIME_TYPES = ["image/jpeg", "image/png", "image/gif"]
+ACCEPTABLE_IMAGE_TYPES = ["jpeg", "png", "gif"]
+IMAGE_URL = cgi.FieldStorage().getvalue("imageURL")
 
 
 def main():
@@ -20,8 +23,24 @@ def main():
 
 def validate():
     try:
-        imageURL = cgi.FieldStorage().getvalue("imageURL")
-        r = requests.get(imageURL, stream=True, headers={"User-Agent": USER_AGENT})
+        r = requests.head(IMAGE_URL, headers=HEADERS)
+    except:
+        return "Could not fetch file headers"
+
+    try:
+        if int(r.headers["Content-Length"]) > MAX_FILE_SIZE_IN_BYTES:
+            return "Image header reports file too large"
+    except:
+        return "Image headers do not contain Content-Length"
+
+    try:
+        if r.headers["Content-Type"] not in ACCEPTABLE_MIME_TYPES:
+            return "Image header reports unacceptable mime type"
+    except:
+        return "Image headers do not contain Content-Type"
+
+    try:
+        r = requests.get(IMAGE_URL, stream=True, headers=HEADERS)
     except:
         return "Could not fetch file"
 
